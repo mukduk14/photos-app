7a88caa180ed2c3a28e45b65dd8c7e42c3d0b560
Rails Udemy Course <punjabishan@gmail.com>
2016-01-22 06:57:02 +0000

    Debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'shan-photo-app.herokuapp.com', :protocol => 'https'}
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
